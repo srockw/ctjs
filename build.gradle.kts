4f4b22333624f35493bc5b68d94ed919e63f026a
srockw <136873475+srockw@users.noreply.github.com>
2026-06-21 19:01:51 -0300

    build: re-add full property for typings

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
+if (!project.hasProperty("full")) {
+    project.gradle.startParameter.excludedTaskNames.add("kspKotlin")
+}
+
 version = property("mod_version").toString()
 
 repositories {
